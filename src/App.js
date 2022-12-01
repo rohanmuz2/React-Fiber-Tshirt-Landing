3b7606c9a21f7cc1767d25e068c0d635c28cd14a
Nisaar <rohan.muz2@gmail.com>
2022-12-02 01:16:50 +0530

    Version 1.1

--- src/App.js
+++ src/App.js
@@ -1,56 +1,81 @@
 import "./index.css";
-import { Suspense, useRef, useState } from "react";
+import { Suspense, useRef } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, useGLTF, Stars } from "@react-three/drei";
+import { OrbitControls, useGLTF, Stars, useAnimations } from "@react-three/drei";
 
-function Model({ ...props }) {
-  const group = useRef();
-  const { nodes, materials } = useGLTF("/shoe.gltf");
+// function Model({ ...props }) {
+//   const group = useRef();
+//   const { nodes, materials } = useGLTF("/shoe.gltf");
+//   return (
+//     <group ref={group} {...props} dispose={null} scale={3}>
+//       <mesh
+//         geometry={nodes.shoe.geometry}
+//         material={materials.laces}
+//         material-color={props.customColors.setStripes}
+//       />
+//       <mesh
+//         geometry={nodes.shoe_1.geometry}
+//         material={materials.mesh}
+//         material-color={props.customColors.mesh}
+//       />
+//       <mesh
+//         geometry={nodes.shoe_2.geometry}
+//         material={materials.caps}
+//         material-color={props.customColors.soul}
+//       />
+//       <mesh
+//         geometry={nodes.shoe_3.geometry}
+//         material={materials.inner}
+//         material-color={props.customColors.soul}
+//       />
+//       <mesh
+//         geometry={nodes.shoe_4.geometry}
+//         material={materials.sole}
+//         material-color={props.customColors.soul}
+//       />
+//       <mesh
+//         geometry={nodes.shoe_5.geometry}
+//         material={materials.stripes}
+//         material-color={props.customColors.stripes}
+//       />
+//       <mesh
+//         geometry={nodes.shoe_6.geometry}
+//         material={materials.band}
+//         material-color={props.customColors.stripes}
+//       />
+//       <mesh
+//         geometry={nodes.shoe_7.geometry}
+//         material={materials.patch}
+//         material-color={props.customColors.soul}
+//       />
+//     </group>
+//   );
+// }
+
+function Model(props) {
+  const { nodes, materials } = useGLTF('/testshirt.gltf')
   return (
-    <group ref={group} {...props} dispose={null} scale={3}>
-      <mesh
-        geometry={nodes.shoe.geometry}
-        material={materials.laces}
-        material-color={props.customColors.setStripes}
-      />
-      <mesh
-        geometry={nodes.shoe_1.geometry}
-        material={materials.mesh}
-        material-color={props.customColors.mesh}
-      />
-      <mesh
-        geometry={nodes.shoe_2.geometry}
-        material={materials.caps}
-        material-color={props.customColors.soul}
-      />
-      <mesh
-        geometry={nodes.shoe_3.geometry}
-        material={materials.inner}
-        material-color={props.customColors.soul}
-      />
-      <mesh
-        geometry={nodes.shoe_4.geometry}
-        material={materials.sole}
-        material-color={props.customColors.soul}
-      />
-      <mesh
-        geometry={nodes.shoe_5.geometry}
-        material={materials.stripes}
-        material-color={props.customColors.stripes}
-      />
-      <mesh
-        geometry={nodes.shoe_6.geometry}
-        material={materials.band}
-        material-color={props.customColors.stripes}
-      />
-      <mesh
-        geometry={nodes.shoe_7.geometry}
-        material={materials.patch}
-        material-color={props.customColors.soul}
-      />
+    <group {...props} dispose={null}>
+      <mesh geometry={nodes.Cube.geometry} material={materials.Material} />
+      <mesh geometry={nodes.Plane.geometry} material={materials.shirt} rotation={[0, 0, -Math.PI / 2]} scale={0.09} />
     </group>
-  );
+  )
 }
+// function Model(props) {
+//   const group = useRef()
+//   const { nodes, materials, animations } = useGLTF("/tee2.gltf");
+//   const { actions } = useAnimations(animations, group)
+//   return (
+//     <group ref={group} {...props} dispose={null}>
+//       <group name="Scene">
+//         <mesh name="Cube" geometry={nodes.Cube.geometry} material={materials.Material} />
+//         <mesh name="Seems" geometry={nodes.Seems.geometry} material={materials.Material} position={[0, -0.77, -0.02]} scale={0.45} />
+//         <mesh name="T-Shirt" geometry={nodes['T-Shirt'].geometry} material={materials.Material} position={[0, -0.6, 0]} scale={0.03} />
+//       </group>
+//     </group>
+//   )
+// }
+
 
 // function Model({ ...props }) {
 //   const group = useRef();
@@ -88,7 +113,7 @@ function App() {
             position={[10, 15, 10]}
             castShadow
           />
-          <Stars
+          <Stars  
             radius={300}
             depth={60}
             count={20000}
@@ -96,9 +121,10 @@ function App() {
             saturation={0}
             fade={true}
           />
-          <Model
+          <Model/>
+          {/* <Model
             customColors={{ mesh: "red", stripes: "blue", soul: "green" }}
-          />
+          /> */}
           <OrbitControls
             enablePan={true}
             enableZoom={true}
